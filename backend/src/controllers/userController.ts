9c769ddf0771aed6c77f75e5fdae03c8db6ddb1e
Silvia Tuiran <silviatuiranb@gmail.com>
2025-01-30 17:05:29 +0100

    trip controller

--- backend/src/controllers/userController.ts
+++ backend/src/controllers/userController.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from 'express';
-import User from '../models/User';
+import User from "../models/user"
 
 export const registerUser = async (req: Request, res: Response) => {
     try {
@@ -8,28 +8,17 @@ export const registerUser = async (req: Request, res: Response) => {
             res.status(400).json({ error: "Todos los campos son obligatorios" });
             return 
         }
-
-      
         const existingUser = await User.findOne({ email });
         if (existingUser) {
             res.status(400).json({ error: "El email ya está registrado. Usa otro." });
             return 
         }
-
         const newUser = new User({ userName, email, password });
-
-        const savedUser = await newUser.save();
-        
-        if(!savedUser) {
-            res.status(400).json({ error: "Error al registrar usuario" });
-            return 
-        }
-        
+        await newUser.save();
         res.status(201).json({ message: "Usuario registrado correctamente" });
-        return
-     } catch (error: unknown) {
+     } catch (error) {
             console.error("Error en registerUser:", error); 
-            res.status(500).json({ message: "Error al registrar usuario", details: error });
+            res.status(500).json({ message: "Error al registrar usuario" });
             return
         }
         
@@ -39,28 +28,20 @@ export const updateUser = async (req: Request, res: Response) => {
     try {
         const { id } = req.params;
         const { userName, email, password } = req.body;
-        if (!userName || !email || !password) {
-            res.status(400).json({ error: "Todos los campos son obligatorios" });
-            return 
-        }
-
-        const existingUser = await User.findOne({ _id: id });
+        const existingUser = await User.findById(id);
         if (!existingUser) {
             res.status(400).json({ error: "El usuario no existe" });
             return 
         }
-
-        const editedUser = await User.updateOne({ _id: id }, { userName, email, password });
-        if (!editedUser) {
-            res.status(400).json({ error: "Error al actualizar usuario" });
-            return 
-        }
-
-        res.status(200).json({ message: "Usuario actualizado correctamente" });
-        return
-    } catch (error: unknown) {
+        const updateFields : {userName?:string, email?:string; password?:string} = {};
+        if (userName) updateFields.userName = userName;
+        if (email) updateFields.email = email;
+        if (password) updateFields.password = password;
+        const updatedUser = await User.findByIdAndUpdate(id,updateFields, {new:true})
+        res.status(200).json({messaje: "Usuario actualizado correctamente",user: updatedUser})
+    } catch (error) {
         console.error("Error en editUser:", error);
-        res.status(500).json({ message: "Error al actualizar usuario", details: error });
+        res.status(500).json({ message: "Error al actualizar usuario" });
         return
     }
 }
@@ -68,24 +49,16 @@ export const updateUser = async (req: Request, res: Response) => {
 export const deleteUser = async (req: Request, res: Response) => {
     try {
         const { id } = req.params;
-        const existingUser = await User.findOne({ _id: id });
+        const deletedUser = await User.findByIdAndDelete(id);
 
-        if (!existingUser) {
-            res.status(400).json({ error: "El usuario no existe" });
-            return 
-        }
-
-        const deletedUser = await User.deleteOne({ _id: id });
         if (!deletedUser) {
-            res.status(400).json({ error: "Error al eliminar usuario" });
+            res.status(404).json({ error: "El usuario no existe" });
             return 
         }
-        
         res.status(200).json({ message: "Usuario eliminado correctamente" });
-        return;
-    } catch (error: unknown) {
+    } catch (error) {
         console.error("Error en deleteUser:", error);
-        res.status(500).json({ message: "Error al eliminar usuario", details: error });
+        res.status(500).json({ message: "Error al eliminar usuario"});
         return;
     }
 }
@@ -95,14 +68,13 @@ export const getUserById = async (req: Request, res: Response ) => {
         const { id } = req.params;
         const user = await User.findById(id);
         if (!user) {
-            res.status(400).json({ error: "El usuario no existe" });
+            res.status(404).json({ error: "El usuario no existe" });
             return 
         }
         res.status(200).json(user);
-        return 
-    } catch (error: unknown) {
+    } catch (error) {
         console.error("Error en getUserById:", error);
-        res.status(500).json({ message: "Error al obtener usuario", details: error });
+        res.status(500).json({ message: "Error al obtener usuario"});
         return;        
     }
 }
@@ -112,14 +84,13 @@ export const getUserByEmail = async (req: Request, res: Response ) => {
         const { email } = req.params;
         const user = await User.findOne({ email });
         if (!user) {
-            res.status(400).json({ error: "El usuario no existe" });
+            res.status(404).json({ error: "El usuario no existe" });
             return 
         }
         res.status(200).json(user);        
-        return 
-    } catch (error: unknown) {
+    } catch (error) {
         console.error("Error en getUserByEmail:", error);
-        res.status(500).json({ message: "Error al obtener usuario", details: error });
+        res.status(500).json({ message: "Error al obtener usuario"});
         return;        
         
     }
@@ -129,14 +100,14 @@ export const getAllUsers = async (_req: Request, res: Response) => {
     try {
         const users = await User.find();
         if (!users || users.length === 0) {
-            res.status(400).json({ error: "No hay usuarios registrados" });
-            return 
+            res.status(200).json({ error: "No hay usuarios registrados", users: [] });
+            
         }
         res.status(200).json(users);
-        return 
-    } catch (error: unknown) {
+ 
+    } catch (error) {
         console.error("Error en getAllUsers:", error);
-        res.status(500).json({ message: "Error al obtener usuarios", details: error });
+        res.status(500).json({ message: "Error al obtener usuarios"});
         return;
         
     }

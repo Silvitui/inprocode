58247aaf29a0a47b418a40af263f6d4d569a0edd
Silvitui <silviatuiranb@gmail.com>
2025-03-23 12:35:29 +0100

    Inject runtime environment variables with ngx-build-plus

---
+++ frontend/src/assets/env.js
@@ -0,0 +1,5 @@
+window['env'] = {
+    backendUrl: 'https://planitgoeco-backend.onrender.com/api',
+    mapboxToken: 'pk.eyJ1Ijoic2lsdml0dWk4IiwiYSI6ImNtN2IydzZtMzBkOTcyaXM4eW54ejkxdmgifQ.47utSyqhFdsuM3fPhhB--Q'
+  };
+  
--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  mapboxToken: 'pk.eyJ1Ijoic2lsdml0dWk4IiwiYSI6ImNtN2IydzZtMzBkOTcyaXM4eW54ejkxdmgifQ.47utSyqhFdsuM3fPhhB--Q',
-  backendUrl: 'https://planitgoeco-backend.onrender.com'
+  backendUrl: (window as any)['env']['backendUrl'],
+  mapboxToken: (window as any)['env']['mapboxToken']
 };

935acc51290f38708a1f85235133409b3fe1bdca
Silvitui <silviatuiranb@gmail.com>
2025-03-23 12:23:01 +0100

    Hardcode backend URL for production deploy

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,6 +1,5 @@
 export const environment = {
-    production: true,
-    mapboxToken: process.env['NG_APP_MAPBOX_API_KEY'] || '',
-    backendUrl: process.env['NG_APP_BACKEND_URL'] || ''
-  };
-  
+  production: true,
+  mapboxToken: 'pk.eyJ1Ijoic2lsdml0dWk4IiwiYSI6ImNtN2IydzZtMzBkOTcyaXM4eW54ejkxdmgifQ.47utSyqhFdsuM3fPhhB--Q',
+  backendUrl: 'https://planitgoeco-backend.onrender.com'
+};

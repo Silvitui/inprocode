029131b3c1db0e6ee49af7043d78522ccfc6174f
Silvitui <silviatuiranb@gmail.com>
2025-03-23 21:13:29 +0100

    Actualiza contenido antes del deploy

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://planitgoeco-backend.onrender.com/api',  // Backend en Render
-  mapboxToken: 'pk.eyJ1Ijoic2lsdml0dWk4IiwiYSI6ImNtN2IydzZtMzBkOTcyaXM4eW54ejkxdmgifQ.47utSyqhFdsuM3fPhhB--Q'
+  backendUrl: (window as any)['env']['backendUrl'],
+  mapboxToken: (window as any)['env']['mapboxToken']
 };
